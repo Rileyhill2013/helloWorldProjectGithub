be7ea32aa65b2b9d1cca6a29dcd6af2c9b7d46ca
Riley Hill <059667@swcsd.us>
2019-10-29 12:43:00 -0400

    Initial Commit

---
+++ helloWorldProjectGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloWorldProjectGithub
+//
+//  Created by Riley Hill on 10/29/19.
+//  Copyright © 2019 Riley Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
